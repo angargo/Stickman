d6e9d22d7c7a30d57c2a68cb1615e4d4d1efdf60
Angel Garcia Gomez <angelgarciagmz@gmail.com>
2016-05-29 15:53:16 +0200

    Add Unity project

---
+++ Stickman 1/Assets/CameraPosition.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour {
+  
+  private Player player;
+  private Vector3 drag;
+  private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+    player = GameObject.FindObjectOfType<Player>();
+    offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    CheckCustomEvents();
+	}
+  
+  void CheckCustomEvents() {
+    if (Input.GetMouseButtonDown(0)) OnMouseLeftDown();
+    if (Input.GetMouseButton(0)) OnMouseLeftDrag();
+    if (Input.GetMouseButtonUp(0)) OnMouseLeftUp();
+    
+  }
+  
+  void OnMouseLeftDrag() {
+    Vector3 diff = Input.mousePosition - drag;
+    if (Input.GetKey(KeyCode.LeftShift)) {
+      Debug.Log("Vertical drag");
+      transform.RotateAround(player.transform.position, transform.TransformDirection(Vector3.right), diff.y);
+    } else {
+      transform.RotateAround(player.transform.position, Vector3.forward, diff.x);
+    }
+    offset = transform.position - player.transform.position;
+    
+    drag = Input.mousePosition;
+  }
+  
+  void OnMouseLeftDown() {
+    drag = Input.mousePosition;
+  }
+  
+  void OnMouseLeftUp() {
+  }
+  
+  public void FollowPlayer(Vector3 position) {
+    transform.position = offset + player.transform.position;
+  }
+}
---
+++ Stickman 1/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+  
+  public float speed = 1;
+  
+  private Vector3 direction;
+  private Vector3 targetPosition;
+  private bool isMoving = false;
+  
+  private Animator animator;
+  private CameraPosition cameraPosition;
+
+	// Use this for initialization
+	void Start () {
+    isMoving = false;
+    animator = this.GetComponent<Animator>();
+    cameraPosition = GameObject.FindObjectOfType<CameraPosition>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (isMoving) {
+      Vector3 newPosition = this.transform.position + direction * Mathf.Min(speed * Time.deltaTime, (this.transform.position - targetPosition).magnitude);
+      RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(Camera.main.ScreenPointToRay(Camera.main.WorldToScreenPoint(newPosition)));
+      bool collision = false;
+      foreach (RaycastHit2D hit in hits) {
+        if (hit.collider.gameObject.tag == "Obstacle") {
+          collision = true;
+          break;
+        }
+      }
+      if (!collision) this.transform.position = newPosition;
+      if (collision || Vector3.Dot(targetPosition - this.transform.position, direction) < Mathf.Epsilon) {
+        isMoving = false;
+        animator.SetBool("isWalking", false);
+      }
+    }
+    cameraPosition.FollowPlayer(transform.position);
+	}
+  
+  void FixedUpdate () {
+    
+	}
+  
+  public void MoveToPosition(Vector3 position) {
+    targetPosition = position;
+    direction = (targetPosition - this.transform.position);
+    direction.Normalize();
+    isMoving = true;
+    animator.SetBool("isWalking", true);
+  }
+}
