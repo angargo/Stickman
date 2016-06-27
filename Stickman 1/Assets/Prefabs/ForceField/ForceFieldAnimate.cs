b884900b0b3b8b5c38a3a150e4e93521f0e93434
pernilsalat <jazzel88@gmail.com>
2016-06-27 20:39:49 +0200

    MagicMirror v1.0

---
+++ Stickman 1/Assets/Prefabs/ForceField/ForceFieldAnimate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceFieldAnimate : MonoBehaviour {
+
+    public float scrollspeed = 0.5f;
+
+    private bool U = true;
+    private bool V = false;
+
+    private float offset;
+
+    public Material mat;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset = Time.time * scrollspeed % 1;
+
+        if (U & V) mat.mainTextureOffset = new Vector2(offset, offset);
+        else if (U) mat.mainTextureOffset = new Vector2(0, offset);
+        else if (V) mat.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
---
+++ Stickman 1/Assets/Scripts/Camera/ScreenSpaceDistortionEffect.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSpaceDistortionEffect : MonoBehaviour
+{
+    RenderTexture shieldRT;
+    RenderTexture screenRT;
+    Camera distortCam;
+    Camera mainCam;
+    Material effectMaterial;
+
+    void Awake()
+    {
+        screenRT = new RenderTexture(Screen.width, Screen.height, 0, RenderTextureFormat.Default);
+        screenRT.wrapMode = TextureWrapMode.Repeat;
+
+        shieldRT = new RenderTexture(Screen.width / 4, Screen.height / 4, 0, RenderTextureFormat.Default);
+        shieldRT.wrapMode = TextureWrapMode.Repeat;
+
+        effectMaterial = new Material(Shader.Find("Custom/Composite"));
+
+        mainCam = GetComponent<Camera>();
+        mainCam.SetTargetBuffers(screenRT.colorBuffer, screenRT.depthBuffer);
+
+        distortCam = new GameObject("DistortionCam").AddComponent<Camera>();
+        distortCam.enabled = false;
+    }
+
+    void OnPostRender()
+    {
+        distortCam.CopyFrom(mainCam);
+        distortCam.backgroundColor = Color.grey;
+        distortCam.cullingMask = 1 << LayerMask.NameToLayer("Shield");
+        distortCam.targetTexture = shieldRT;
+        distortCam.Render();
+
+        effectMaterial.SetTexture("_DistortionTex", shieldRT);
+        Graphics.Blit(screenRT, null, effectMaterial);
+    }
+
+}
