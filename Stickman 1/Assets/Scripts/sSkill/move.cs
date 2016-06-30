9a2e8526d2a0f7f99b664e3fa5b6078fe2517586
pernilsalat <jazzel88@gmail.com>
2016-06-30 18:24:58 +0200

    Fireball x2

---
+++ Stickman 1/Assets/Scripts/sSkill/move.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+    public float v = 2;
+    private Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+        GetComponent<Rigidbody>().velocity = new Vector3( v, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x > 10) transform.position = pos;
+	}
+}
